b1ce10f1de99793b34f3b33ff9c1019907d53837
Jon <jonamar@gmail.com>
2022-08-19 11:42:12 -0400

    starting to theme tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,33 @@ module.exports = {
     "./**/*.{html}",
     "./index.html"
   ],
+  // more info https://tailwindcss.com/docs/theme
   theme: {
-    extend: {},
+    // colors: https://tailwindcss.com/docs/customizing-colors
+    colors: {
+      'blue': '#1fb6ff',
+      //'purple': '#7e5bef',
+      'pink': '#ff49db',
+      //'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
   plugins: [],
 }
