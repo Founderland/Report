935868f3e4b22fc150d9d1b3658bf0bfc8cac356
Jon <jonamar@gmail.com>
2022-08-19 08:10:23 -0400

    fix tw content location config to detect index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} **/
 module.exports = {
-  content: ["./**/*.{html}"],
+  content: [
+    "./**/*.{html}",
+    "./index.html"
+  ],
   theme: {
     extend: {},
   },
