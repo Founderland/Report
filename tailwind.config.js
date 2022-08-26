85d56ba755b8b41e4293810653ae333957667ce3
Jon <jonamar@gmail.com>
2022-08-26 10:56:46 -0400

    ran tw build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} **/
 module.exports = {
   content: [
-    "./**/*.{html}",
+    "./**/*.html",
     "./index.html"
   ],
   // more info https://tailwindcss.com/docs/theme
