b5d583fc56b8ff0218d8747c32d98749b56917d2
Jon <jonamar@gmail.com>
2022-09-05 20:30:05 +0000

    troubleshooting scrollspy

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,25 +14,25 @@ $(".overlay").click(function() {
 
 // Scrollspy 
 // source: https://codepen.io/acarlie/pen/LKmORw?editors=1010
-var sectionIds = $('nav a');
+// var sectionIds = $('nav a');
 
-$(document).scroll(function(){
-    sectionIds.each(function(){
+// $(document).scroll(function(){
+//     sectionIds.each(function(){
 
-        var container = $(this).attr('href');
-        var containerOffset = $(container).offset().top;
-        var containerHeight = $(container).outerHeight();
-        var containerBottom = containerOffset + containerHeight;
-        var scrollPosition = $(document).scrollTop();
+//         var container = $(this).attr('href');
+//         var containerOffset = $(container).offset().top;
+//         var containerHeight = $(container).outerHeight();
+//         var containerBottom = containerOffset + containerHeight;
+//         var scrollPosition = $(document).scrollTop();
 
-        if(scrollPosition < containerBottom - 20 && scrollPosition >= containerOffset - 20){
-            $(this).addClass('active');
-        } else{
-            $(this).removeClass('active');
-        };
+//         if(scrollPosition < containerBottom - 20 && scrollPosition >= containerOffset - 20){
+//             $(this).addClass('active');
+//         } else{
+//             $(this).removeClass('active');
+//         };
 
-    });
-});
+//     });
+// });
 
 
 
