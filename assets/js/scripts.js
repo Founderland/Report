a7a272df17f2d6adaa1c0937de7e08220557d8d8
Jon <jonamar@gmail.com>
2022-09-24 13:05:44 +0000

    commented out charts js examples js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -54,57 +54,52 @@ const swiper = new Swiper('.swiper', {
 
 
 // Charts with charts.js -- Deprecated
-var colors =  [
-    '#0063E2', // blue
-    '#F6331C', // red
-    '#EE93B5', // pink
-    '#D7FB03', // lime
-    '#000' // black
-];
+// var colors =  [
+//     '#0063E2', // blue
+//     '#F6331C', // red
+//     '#EE93B5', // pink
+//     '#D7FB03', // lime
+//     '#000' // black
+// ];
 
-// Chart.defaults.global.data.datasets.backgroundColor = true;
-// Chart.defaults.global.animationSteps = 160;
+// const bar1 = document.getElementById('barChart').getContext('2d');
+// const barChart = new Chart(bar1, {
+//     type: 'bar',
+//     data: {
+//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//         datasets: [{
+//             data: [12, 19, 3, 5, 2, 3],
+//             label: '# of Votes',
+//             backgroundColor: colors,
+//             borderColor: colors,
+//             borderWidth: 0
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             y: {
+//                 beginAtZero: true
+//             }
+//         }
+//     }
+// });
 
-
-const bar1 = document.getElementById('barChart').getContext('2d');
-const barChart = new Chart(bar1, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            data: [12, 19, 3, 5, 2, 3],
-            label: '# of Votes',
-            backgroundColor: colors,
-            borderColor: colors,
-            borderWidth: 0
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
-
-const pie1 = document.getElementById('pieChart').getContext('2d');
-const pieChart = new Chart(pie1, {
-    type: 'pie',
-    data: {
-        labels: ['Everyone else', 'Women of color founders'],
-        datasets: [{
-            data: [99.5, 0.5,],
-            //label: '# of Votes',
-            backgroundColor: colors,
-            borderColor: colors,
-            borderWidth: 0
-        }]
-    },
-    options: {
+// const pie1 = document.getElementById('pieChart').getContext('2d');
+// const pieChart = new Chart(pie1, {
+//     type: 'pie',
+//     data: {
+//         labels: ['Everyone else', 'Women of color founders'],
+//         datasets: [{
+//             data: [99.5, 0.5,],
+//             backgroundColor: colors,
+//             borderColor: colors,
+//             borderWidth: 0
+//         }]
+//     },
+//     options: {
        
-    }
-});
+//     }
+// });
 
 
 }); // end of document ready
