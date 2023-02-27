173971b3b48aaba8d041deed002a9179776cd606
Victor Isidoro <31276085+vtr84@users.noreply.github.com>
2023-02-27 20:17:46 +0000

    cookie consent correction

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -56,54 +56,6 @@ $(document).ready(function () {
       crossFade: true,
     },
   })
-
-  // Charts with charts.js -- Deprecated
-  // var colors =  [
-  //     '#0063E2', // blue
-  //     '#F6331C', // red
-  //     '#EE93B5', // pink
-  //     '#D7FB03', // lime
-  //     '#000' // black
-  // ];
-
-  // const bar1 = document.getElementById('barChart').getContext('2d');
-  // const barChart = new Chart(bar1, {
-  //     type: 'bar',
-  //     data: {
-  //         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-  //         datasets: [{
-  //             data: [12, 19, 3, 5, 2, 3],
-  //             label: '# of Votes',
-  //             backgroundColor: colors,
-  //             borderColor: colors,
-  //             borderWidth: 0
-  //         }]
-  //     },
-  //     options: {
-  //         scales: {
-  //             y: {
-  //                 beginAtZero: true
-  //             }
-  //         }
-  //     }
-  // });
-
-  // const pie1 = document.getElementById('pieChart').getContext('2d');
-  // const pieChart = new Chart(pie1, {
-  //     type: 'pie',
-  //     data: {
-  //         labels: ['Everyone else', 'Women of color founders'],
-  //         datasets: [{
-  //             data: [99.5, 0.5,],
-  //             backgroundColor: colors,
-  //             borderColor: colors,
-  //             borderWidth: 0
-  //         }]
-  //     },
-  //     options: {
-
-  //     }
-  // });
 }) // end of document ready
 
 // progress bar for scroll
@@ -124,65 +76,68 @@ const scrollProgress = () => {
   }
 }
 
-// Charts with chart.js
-
-// 'rgba(238, 147, 181, 1)',
-//         'rgba(0, 99, 226, 1)',
-//         'rgba(223, 248, 81, 1)',
-
+function createCookie(name, value, days) {
+  var expires = ''
+  if (days) {
+    var date = new Date()
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000)
+    expires = '; expires=' + date.toUTCString()
+  }
+  document.cookie = name + '=' + value + expires + '; path=/'
+}
 function readCookie(name) {
-  var nameEq = name + '='
-  var cookies = document.cookie.split(';')
-  for (var i = 0; i < cookies.length; i++) {
-    var c = cookies[i]
+  var nameEQ = name + '='
+  var ca = document.cookie.split(';')
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i]
     while (c.charAt(0) == ' ') c = c.substring(1, c.length)
-    if (c.indexOf(nameEq) == 0) return c.substring(nameEq.length, c.length)
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length)
   }
   return null
 }
+function eraseCookie(name) {
+  createCookie(name, '', -1)
+}
 
-function recordConsent() {
-  var date = new Date()
-  // One year from now
-  date.setTime(date.getTime() + 365 * 24 * 3600 * 1000)
-  var suffix = '; expires=' + date.toUTCString() + '; path=/'
+if (readCookie('cookie-notice-option') == 'true') {
+  function loadScriptAsync(scriptSrc, callback) {
+    if (typeof callback !== 'function') {
+      throw new Error('Not a valid callback for async script load')
+    }
+    var script = document.createElement('script')
+    script.onload = callback
+    script.src = scriptSrc
+    document.head.appendChild(script)
+  }
 
-  document.cookie =
-    'cookie_consent_analytics=' +
-    document.getElementById('cookie-consent-analytics').checked +
-    suffix
-  document.cookie = 'cookie_consent_cleared=true' + suffix
+  /* This is the part where you call the above defined function and "calls back" your code which gets executed after the script has loaded */
 
-  gtag('consent', 'update', {
-    analytics_storage: document.getElementById('cookie-consent-analytics')
-      .checked
-      ? 'granted'
-      : 'denied',
+  loadScriptAsync(
+    'https://www.googletagmanager.com/gtag/js?id=G-Q749X5MH40',
+    function () {
+      window.dataLayer = window.dataLayer || []
+      function gtag() {
+        dataLayer.push(arguments)
+      }
+      document.getElementById('c-banner').style.display = 'none'
+      gtag('js', new Date())
+      gtag('config', 'G-Q749X5MH40', { anonymize_ip: true })
+    }
+  )
+} else if (readCookie('cookie-notice-option') == 'false') {
+  document.getElementById('c-banner').style.display = 'none'
+}
+
+document
+  .getElementById('c-banner-accept')
+  .addEventListener('click', function () {
+    createCookie('cookie-notice-option', 'true', 31)
+    document.getElementById('c-banner').style.display = 'none'
+    location.reload()
   })
-}
 
-window.dataLayer = window.dataLayer || []
-function gtag() {
-  dataLayer.push(arguments)
-}
-gtag('consent', 'default', { ad_storage: 'denied' })
-gtag('consent', 'default', {
-  analytics_storage: 'denied',
-  region: ['EU', 'UK'],
+document.getElementById('c-banner-deny').addEventListener('click', function () {
+  createCookie('cookie-notice-option', 'false', 31)
+  document.getElementById('c-banner').style.display = 'none'
+  location.reload()
 })
-
-if (readCookie('cookie_consent_cleared')) {
-  document.getElementById('cookie-consent').style.display = 'none'
-  document.getElementById('cookie-consent-analytics').checked =
-    readCookie('cookie_consent_analytics') == 'true'
-
-  gtag('consent', 'update', {
-    analytics_storage: document.getElementById('cookie-consent-analytics')
-      .checked
-      ? 'granted'
-      : 'denied',
-  })
-}
-
-gtag('js', new Date())
-gtag('config', 'G-Q749X5MH40')
